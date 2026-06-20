f5864789eab3b1c4312ebf613b7da5b8548ca50d
umesh bharude <umeshbharude253@gmail.com>
2026-06-20 11:29:46 +0530

    Refactor print statement in app.py
    Fix indentation and remove commented print statement.

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ num2 = 21
 sum = num1 + num2
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum)
